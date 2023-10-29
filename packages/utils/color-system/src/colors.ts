84ebf5adfd0185de7fde7192731b1af05a75724c
ainochi-kor <kthrkdals@gmail.com>
2023-10-29 22:47:09 +0900

    update: package name

--- packages/utils/color-system/src/colors.ts
+++ packages/utils/color-system/src/colors.ts
@@ -1,4 +1,4 @@
-import { createDefaultColors, createColorTheme } from "@neos-ui/color-utils";
+import { createDefaultColors, createColorTheme } from "@neos-utils/color-utils";
 
 export const defaultColorSystem = createDefaultColors();
 export const defaultColorTheme = createColorTheme(defaultColorSystem);
